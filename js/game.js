8d1b48648cc413c92875ee5a590a850793257650
Quinten <144217470+quintendoes@users.noreply.github.com>
2024-05-08 11:57:58 +0200

    chore: Update player movement and add enemy image

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-const x = 100;
-let y = 100;
+let x = 100;
+let y = 503;
 const width = 100;
 const height = 100;
 
@@ -11,15 +11,30 @@ backgroundImage.src = '../assets/images/classroom.jpeg';
 const playerImage = new Image();
 playerImage.src = '../sprite/player-normal.png';
 
+const enemyImage = new Image();
+enemyImage.src = '../sprite/enemy-idleleft.png';
+
 const plankImage = new Image();
 plankImage.src = '../sprite/plank-sprite.png';
 
 document.addEventListener('keydown', function(event) {
-    if (event.code === 'Space' || event.code === 'ArrowUp') {
+    if (event.code === 'KeyW' || event.code === 'ArrowUp') {
         playerImage.src = '../sprite/player-jump.png';
+        y -= 20;
         setTimeout(function() {
             playerImage.src = '../sprite/player-normal.png';
+            y += 20;
         }, 500);
+    }
+
+    if (event.code === 'ArrowRight' || event.code === 'KeyD') {
+        playerImage.src = '../sprite/player-right.png';
+        x += 10;
+    }
+
+    if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
+        playerImage.src = '../sprite/player-left.png';
+        x -= 10;
     }
 });
 
@@ -27,7 +42,16 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(playerImage, x, y, width, height);
-    ctx.drawImage(plankImage, 125, 195, 50, 50);
+    ctx.drawImage(enemyImage, 400, 505, 100, 100);
+
+    // Planks
+    ctx.drawImage(plankImage, 125, 600, 50, 50);
+    ctx.drawImage(plankImage, 175, 600, 50, 50);
+    ctx.drawImage(plankImage, 225, 600, 50, 50);
+    ctx.drawImage(plankImage, 275, 600, 50, 50);
+    ctx.drawImage(plankImage, 325, 600, 50, 50);
+    ctx.drawImage(plankImage, 375, 600, 50, 50);
+    ctx.drawImage(plankImage, 425, 600, 50, 50);
 }
 
 backgroundImage.onload = function() {
@@ -35,6 +59,10 @@ backgroundImage.onload = function() {
 };
 
 playerImage.onload = function() {
+    draw();
+};
+
+enemyImage.onload = function() {
     draw();
 };
 
