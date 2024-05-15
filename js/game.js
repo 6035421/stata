8b9eddc4a4b9ec511ae843242db3e461782380cb
Quinten <144217470+quintendoes@users.noreply.github.com>
2024-05-15 10:07:29 +0200

    chore: Update player movement and add enemy image

--- js/game.js
+++ js/game.js
@@ -1,152 +1,205 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const startButton = document.getElementById('start');
-let x = 100;
-let y = 503;
-const width = 100;
-const height = 100;
+const startTime = performance.now();
+level = 1;
 
-const backgroundImage = new Image();
-backgroundImage.src = '../assets/images/classroom.jpeg';
+function changeLevel(level) {
+    if (level === 1) {
+        console.log('Level 1');
+        let x = 100;
+        let y = 503;
+        const width = 100;
+        const height = 100;
 
-const playerImage = new Image();
-playerImage.src = '../sprite/player-normal.png';
+        const backgroundImage = new Image();
+        backgroundImage.src = '../assets/images/classroom.jpeg';
 
-const enemyImage = new Image();
-enemyImage.src = '../sprite/level1/enemy-idleleft.png';
+        const playerImage = new Image();
+        playerImage.src = '../sprite/player-normal.png';
 
-const plankImage = new Image();
-plankImage.src = '../sprite/plank-sprite.png';
+        const enemyImage = new Image();
+        enemyImage.src = '../sprite/level1/enemy-idleleft.png';
 
-const barrelImage = new Image();
-barrelImage.src = '../sprite/level1/barrel-lay.png';
+        const bossImage = new Image();
+        bossImage.src = '../sprite/level1/level1boss - idle.png';
 
-const barrelgroundImage = new Image();
-barrelgroundImage.src = '../sprite/level1/barrelground.png';
+        const plankImage = new Image();
+        plankImage.src = '../sprite/plank-sprite.png';
 
-const rollbarrelImage = new Image();
-rollbarrelImage.src = '../sprite/level1/barrel-roll.png';
+        const barrelImage = new Image();
+        barrelImage.src = '../sprite/level1/barrel-lay.png';
 
-const rollbarrelImage2 = new Image();
-rollbarrelImage2.src = '../sprite/level1/barrel-roll2.png';
+        const barrelgroundImage = new Image();
+        barrelgroundImage.src = '../sprite/level1/barrelground.png';
 
-const rollbarrelImage3 = new Image();
-rollbarrelImage3.src = '../sprite/level1/barrel-roll3.png';
+        const rollbarrelImage = new Image();
+        rollbarrelImage.src = '../sprite/level1/barrel-roll.png';
 
-const ladderImage = new Image();
-ladderImage.src = '../sprite/ladder.png';
+        const rollbarrelImage2 = new Image();
+        rollbarrelImage2.src = '../sprite/level1/barrel-roll2.png';
 
-document.addEventListener('keydown', function (event) {
-    if (event.code === 'KeyW' || event.code === 'ArrowUp') {
-        playerImage.src = '../sprite/player-jump.png';
-        y -= 20;
-        setTimeout(function () {
-            playerImage.src = '../sprite/player-normal.png';
-            y += 20;
-        }, 500);
+        const rollbarrelImage3 = new Image();
+        rollbarrelImage3.src = '../sprite/level1/barrel-roll3.png';
+
+        const ladderImage = new Image();
+        ladderImage.src = '../sprite/ladder.png';
+
+        document.addEventListener('keydown', function (event) {
+            if (event.code === 'KeyW' || event.code === 'ArrowUp') {
+                playerImage.src = '../sprite/player-jump.png';
+                y -= 20;
+                setTimeout(function () {
+                    playerImage.src = '../sprite/player-normal.png';
+                    y += 20;
+                }, 500);
+            }
+
+            if (event.code === 'ArrowRight' || event.code === 'KeyD') {
+                playerImage.src = '../sprite/player-right.png';
+                x += 10;
+            }
+
+            if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
+                playerImage.src = '../sprite/player-left.png';
+                x -= 10;
+            }
+        });
+
+        function draw() {
+            // Clear the canvas
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Background
+            ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+            // Planks
+            ctx.drawImage(plankImage, 125, 600, 50, 50);
+            ctx.drawImage(plankImage, 175, 600, 50, 50);
+            ctx.drawImage(plankImage, 225, 600, 50, 50);
+            ctx.drawImage(plankImage, 275, 600, 50, 50);
+            ctx.drawImage(plankImage, 325, 600, 50, 50);
+            ctx.drawImage(plankImage, 375, 600, 50, 50);
+            ctx.drawImage(plankImage, 425, 600, 50, 50);
+
+            // Planks Row 2
+            ctx.drawImage(plankImage, 425, 470, 50, 50);
+            ctx.drawImage(plankImage, 475, 470, 50, 50);
+            ctx.drawImage(plankImage, 525, 470, 50, 50);
+            ctx.drawImage(plankImage, 575, 470, 50, 50);
+
+            // Barrelgrounds
+            // Top Row
+            ctx.drawImage(barrelgroundImage, 120, 100, 50, 50);
+            ctx.drawImage(barrelgroundImage, 170, 100, 50, 50);
+            ctx.drawImage(barrelgroundImage, 220, 100, 50, 50);
+            ctx.drawImage(barrelgroundImage, 270, 100, 50, 50);
+            ctx.drawImage(barrelgroundImage, 320, 100, 50, 50);
+            ctx.drawImage(barrelgroundImage, 370, 100, 50, 50);
+
+            // Row 1
+            ctx.drawImage(barrelgroundImage, 1200, 200, 50, 50);
+            ctx.drawImage(barrelgroundImage, 1250, 200, 50, 50);
+            ctx.drawImage(barrelgroundImage, 1300, 200, 50, 50);
+            ctx.drawImage(barrelgroundImage, 1350, 200, 50, 50);
+            ctx.drawImage(barrelgroundImage, 1400, 200, 50, 50);
+            ctx.drawImage(barrelgroundImage, 1450, 200, 50, 50);
+
+            // Barrels
+            ctx.drawImage(barrelImage, 120, 40, 100, 100);
+
+            // Boss
+            ctx.drawImage(bossImage, 120, 10, 100, 100);
+
+            // Enemy
+            ctx.drawImage(enemyImage, 1300, 110, 100, 100);
+
+            // Ladder
+            ctx.drawImage(ladderImage, 400, 520, 100, 100);
+
+            // Player
+            ctx.drawImage(playerImage, x, y, width, height);
+        }
+
+        // Draw function
+        backgroundImage.onload = function () {
+            draw();
+        };
+
+        playerImage.onload = function () {
+            draw();
+        };
+
+        enemyImage.onload = function () {
+            draw();
+        };
+
+        bossImage.onload = function () {
+            draw();
+        };
+
+        plankImage.onload = function () {
+            draw();
+        };
+
+        barrelImage.onload = function () {
+            draw();
+        }
+
+        barrelgroundImage.onload = function () {
+            draw();
+        }
+
+        const endTime = performance.now();
+        const loadTime = endTime - startTime;
+        console.log(`Level 1 geladen in ${loadTime} milliseconden`);
     }
 
-    if (event.code === 'ArrowRight' || event.code === 'KeyD') {
-        playerImage.src = '../sprite/player-right.png';
-        x += 10;
+    // Level 2 in level2.js
+    if (level === 2) {
+        const startTime = performance.now();
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        const backgroundImage = new Image();
+        backgroundImage.src = '../assets/images/park.png';
+
+        const playerImage = new Image();
+        playerImage.src = '../sprite/player-normal.png';
+
+        const notextureImage = new Image();
+        notextureImage.src = '../sprite/notexture.png';
+
+        function draw() {
+            ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+            // Player
+            ctx.drawImage(playerImage, 100, 100, 100, 100);
+
+            // No texture
+            ctx.drawImage(notextureImage, 125, 195, 50, 50);
+            ctx.drawImage(notextureImage, 175, 195, 50, 50);
+            ctx.drawImage(notextureImage, 225, 195, 50, 50);
+        }
+
+        backgroundImage.onload = function () {
+            draw();
+        };
+
+        playerImage.onload = function () {
+            draw();
+        }
+
+        notextureImage.onload = function () {
+            draw();
+        }
+
     }
 
-    if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
-        playerImage.src = '../sprite/player-left.png';
-        x -= 10;
-    }
-});
-
-function draw() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Background
-    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-
-    // Enemy
-    ctx.drawImage(enemyImage, 1300, 110, 100, 100);
-
-    // Planks
-    ctx.drawImage(plankImage, 125, 600, 50, 50);
-    ctx.drawImage(plankImage, 175, 600, 50, 50);
-    ctx.drawImage(plankImage, 225, 600, 50, 50);
-    ctx.drawImage(plankImage, 275, 600, 50, 50);
-    ctx.drawImage(plankImage, 325, 600, 50, 50);
-    ctx.drawImage(plankImage, 375, 600, 50, 50);
-    ctx.drawImage(plankImage, 425, 600, 50, 50);
-
-    // Planks Row 2
-    ctx.drawImage(plankImage, 425, 470, 50, 50);
-    ctx.drawImage(plankImage, 475, 470, 50, 50);
-    ctx.drawImage(plankImage, 525, 470, 50, 50);
-    ctx.drawImage(plankImage, 575, 470, 50, 50);
-
-    // Barrelgrounds
-    ctx.drawImage(barrelgroundImage, 1200, 200, 50, 50);
-    ctx.drawImage(barrelgroundImage, 1250, 200, 50, 50);
-    ctx.drawImage(barrelgroundImage, 1300, 200, 50, 50);
-    ctx.drawImage(barrelgroundImage, 1350, 200, 50, 50);
-    ctx.drawImage(barrelgroundImage, 1400, 200, 50, 50);
-    ctx.drawImage(barrelgroundImage, 1450, 200, 50, 50);
-
-    // Barrels
-    ctx.drawImage(barrelImage, 1200, 140, 100, 100);
-
-    let prevRollBarrelX = 1250;
-    let prevRollBarrelY = 140;
-    let prevRollBarrelWidth = 100;
-    let prevRollBarrelHeight = 100;
-
-    
-    // Roll Barrels
-    ctx.drawImage(rollbarrelImage, prevRollBarrelX, prevRollBarrelY, prevRollBarrelWidth, prevRollBarrelHeight);
-    setTimeout(() => {
-        ctx.clearRect(prevRollBarrelX, prevRollBarrelY, prevRollBarrelWidth, prevRollBarrelHeight);
-        ctx.drawImage(rollbarrelImage2, 1300, 140, 100, 100);
-        prevRollBarrelX = 1300;
-        prevRollBarrelY = 140;
-    }, 1000);
-    setTimeout(() => {
-        ctx.clearRect(prevRollBarrelX, prevRollBarrelY, prevRollBarrelWidth, prevRollBarrelHeight);
-        ctx.drawImage(rollbarrelImage3, 1350, 140, 100, 100);
-        prevRollBarrelX = 1350;
-        prevRollBarrelY = 140;
-    }, 2000);
-
-    // Ladder
-    ctx.drawImage(ladderImage, 400, 520, 100, 100);
-
-    // Player
-    ctx.drawImage(playerImage, x, y, width, height);
-}
-
-// Draw function
-backgroundImage.onload = function () {
-    draw();
+    const endTime = performance.now();
+    const loadTime = endTime - startTime;
+    console.log(`Level 2 geladen in ${loadTime} milliseconden`);
 };
-
-playerImage.onload = function () {
-    draw();
-};
-
-enemyImage.onload = function () {
-    draw();
-};
-
-plankImage.onload = function () {
-    draw();
-};
-
-barrelImage.onload = function () {
-    draw();
-}
-
-barrelgroundImage.onload = function () {
-    draw();
-}
-
-
 
 // Start button
 startButton.addEventListener('click', function () {
@@ -196,3 +249,5 @@ startButton.addEventListener('click', function () {
         });
     }
 });
+
+changeLevel(1);
