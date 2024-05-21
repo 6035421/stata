4ba19c0c9dd0468e06c603cffbabb81fc275b88b
= <lukehoogenboom@gmail.com>
2024-05-21 11:39:21 +0200

    made the game screen fullscreen and added close fullscreen button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const fullscreenButton = document.getElementById("fullscreen");
-const gameCanvas = document.getElementById("gameCanvas");
+const gameCanvas = document.body
 
 fullscreenButton.addEventListener("click", () => {
+    if(fullscreenButton.innerHTML == 'Fullscreen') {
+        fullscreenButton.innerHTML = 'Close Fullscreen';
+
     if (gameCanvas.requestFullscreen) {
         gameCanvas.requestFullscreen();
     } else if (gameCanvas.mozRequestFullScreen) { // Firefox
@@ -11,4 +14,15 @@ fullscreenButton.addEventListener("click", () => {
     } else if (gameCanvas.msRequestFullscreen) { // IE/Edge
         gameCanvas.msRequestFullscreen();
     }
+} else {
+    fullscreenButton.innerHTML = 'Fullscreen';
+
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+      }
+}
 });
--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
 if(window.level == 2) {
-
+    document.body.style.backgroundImage = 'url("../../assets/images/park.png")';
+} else if(window.level == 3) {
+    document.body.style.backgroundImage = 'url("../../assets/images/bar.png")';
+} else if(window.level == 4) {
+    document.body.style.backgroundImage = 'url("../../assets/images/arcade.png")';
+} else if(window.level == 5) {
+    document.body.style.backgroundImage = 'url("../../assets/images/suburbia.png")';
+} else if(window.level == 6) {
+    document.body.style.backgroundImage = 'url("../../assets/images/plant.png")';
 } else {
     document.body.style.backgroundImage = 'url("../../assets/images/classroom.jpeg")';
 }
