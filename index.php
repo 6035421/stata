6eabf05946e0e38d1eeae147f5d5f459d039d05d
= <lukehoogenboom@gmail.com>
2024-06-05 10:23:37 +0200

    - now you are redirected w=to login when you are not logged in and want to view a page that needs logging in

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="/css/style.css">
     <link rel="icon" type="image/x-icon" href="../assets/images/New Piskel.png">
 </head>
+
 <body>
     <!-- Header -->
     <header>
@@ -17,16 +19,25 @@
     <main class='HomeMenuList'>
         <section>
             <article id='logo'>
-            <img src="/assets/images/logo.png" alt="player">
+                <img src="/assets/images/logo.png" alt="player">
             </article>
             <?php echo '    <h1>Welkom admin, ' . $_SESSION['gebruikersnaam'] . '</h1><br />' ?>
 
             <article id='buttons'>
-        <a class='pixel-corners' id='play' href='./game/index.php'>Play</a>
-        <a class='pixel-corners' id='vrienden' href='./vrienden.php'>Vrienden</a>
-        <a class='pixel-corners' id='account' href='./profile.php'>Account</a>
-        </article>
-</section>
+                <a class='pixel-corners' id='play' href='./game/index.php'>Play</a>
+
+                <?php
+        if(isset($_SESSION['password'])) {
+            echo "<a class='pixel-corners' id='vrienden' href='./vrienden.php'>Vrienden</a>";
+            echo "<a class='pixel-corners' id='account' href='./profile.php'>Account</a>";
+        } else {
+            echo "<a class='pixel-corners' id='vrienden' disabled href='./login/index.php'>Vrienden</a>";
+            echo "<a class='pixel-corners' id='account' href='./login/index.php'>Account</a>";
+        }
+        ?>
+
+            </article>
+        </section>
     </main>
 
     <!-- Footer -->
@@ -34,5 +45,6 @@
 
     </footer>
 </body>
-    <script src="/js/menu.js"></script>
+<script src="/js/menu.js"></script>
+
 </html>
