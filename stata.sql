f4a8732e89892b9ebf74ee179c16d0a990923325
= <lukehoogenboom@gmail.com>
2024-06-05 11:47:11 +0200

    !! IMPORTANT SQL STRUCTURE IS UPDATINED IN THIS BRANCH PLEASE IMPORT THE NEW SQL !!
    - started working on friends page

--- stata.sql
+++ stata.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 29 mei 2024 om 10:15
--- Serverversie: 10.4.32-MariaDB
--- PHP-versie: 8.2.12
+-- Generation Time: Jun 05, 2024 at 11:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,65 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(20) NOT NULL,
-  `gebruikersnaam` varchar(225) NOT NULL,
-  `wachtwoord` varchar(225) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Gegevens worden geëxporteerd voor tabel `admin`
---
-
-INSERT INTO `admin` (`id`, `gebruikersnaam`, `wachtwoord`) VALUES
-(0, 'STATA', '$2y$10$bBd3iKOYMe4uSHdatNRM3OwILaCKUqCx2HRQ1zOdwto7.gHQZAvbO');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `gebruikers`
+-- Table structure for table `gebruikers`
 --
 
 CREATE TABLE `gebruikers` (
   `id` int(10) NOT NULL,
   `gebruikersnaam` varchar(225) NOT NULL,
   `wachtwoord` varchar(225) NOT NULL,
-  `foto` varchar(225) DEFAULT '/assets/images/profile-icon.png'
+  `foto` varchar(225) DEFAULT '/assets/images/profile-icon.png',
+  `vrienden` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Gegevens worden geëxporteerd voor tabel `gebruikers`
+-- Dumping data for table `gebruikers`
 --
 
-INSERT INTO `gebruikers` (`id`, `gebruikersnaam`, `wachtwoord`, `foto`) VALUES
-(1, 'Piet', '123', '/assets/images/profile-icon.png'),
-(2, 'stefan', '$2y$10$84fbaAziPXRlNcLRas9Poez4CX1.drWL4VsT0VNCTcqSsYEmqu3rO', '/assets/images/profile-icon.png');
+INSERT INTO `gebruikers` (`id`, `gebruikersnaam`, `wachtwoord`, `foto`, `vrienden`) VALUES
+(1, 'Piet', '123', '/assets/images/profile-icon.png', NULL),
+(2, 'stefan', '$2y$10$84fbaAziPXRlNcLRas9Poez4CX1.drWL4VsT0VNCTcqSsYEmqu3rO', '/assets/images/profile-icon.png', NULL);
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `gebruikers`
+-- Indexes for table `gebruikers`
 --
 ALTER TABLE `gebruikers`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `gebruikers`
+-- AUTO_INCREMENT for table `gebruikers`
 --
 ALTER TABLE `gebruikers`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
--- vrienden.php
+++ vrienden.php
@@ -1,13 +1,50 @@
+<?php 
+include './database/connect.php'; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>STATA</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="icon" type="image/x-icon" href="../assets/images/New Piskel.png">
 </head>
+
 <body>
+    <main>
+        <section class='vrienden'>
+            <article class='pixel-corners buttons'>
+                <input class='pixel-corners' id='zoeken' type='search' placeholder='Zoek naar accounts...'>
+            </article>
+
+            <article class='pixel-corners buttons friends'>
+                <button class='pixel-corners selected' id='mijnVrienden'>Mijn Vrienden - 0</button>
+                <button class='pixel-corners' id='zoekVrienden'>Zoek Vrienden - 2</button>
+            </article>
+
+            <article class='pixel-corners'>
+                <?php
+                if(isset($_SESSION['id'])) {
+                    $sql = "SELECT * FROM gebruikers WHERE id =".$_SESSION['id'];
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+
+    echo $row['vrienden'];
+    if($row['vrienden'] == NULL){
+        echo 'hi';
+    }
+                }
+            }
+                ?>
+            </article>
+            <section>
+    </main>
 </body>
-    <script src="/js/menu.js"></script>
+<script src="/js/menu.js"></script>
+
 </html>
